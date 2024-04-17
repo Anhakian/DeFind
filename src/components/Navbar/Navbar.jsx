4c8c42c105688e84f8caf43f344ebec8a800b680
buikhueanh <anhbui_2026@depauw.edu>
2024-04-16 21:48:55 -0400

    create nav bar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import './Navbar.css'
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <div>
+                <ul className='all-nav-button'>
+                    <li className='title'><a href=''><h1>DeFind</h1></a></li>
+                    <ul className='nav-button'>
+                        <li><a href=''>Home</a></li>
+                        <li><a href=''>About</a></li>
+                        <li><a href=''>Contribute to DeFind</a></li>
+                        <li><a href=''>Resources</a></li>
+                    </ul>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+export default Navbar
