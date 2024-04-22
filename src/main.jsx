5fc1f1b124a262d841e42d16a61ab571302a1cb7
Anhakian <anhnguyenngochoang2003@gmail.com>
2024-04-21 23:50:14 -0400

    fix bootstrap styling

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App.jsx'
 import About from './components/About/About.jsx'
 import Contribute from './components/Contribute/Contribute.jsx';
